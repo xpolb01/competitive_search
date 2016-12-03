1e25c7eaea279bbfbe72ad8e44cc636cb4f7194a
Sam Wheeler <swheeler111@gmail.com>
2016-12-03 12:35:47 -0500

    working on heuristics

--- src/minimax_specs.js
+++ src/minimax_specs.js
@@ -6,7 +6,7 @@ var minimax = minimaxLib.minimax;
 var makeMove = minimaxLib.makeMove;
 var heuristic = minimaxLib.heuristic;
 
-/* 
+/*
    A heuristic takes as input some particular game state.
    and outputs a number indicating how good / bad that state is.
 
@@ -22,7 +22,7 @@ var heuristic = minimaxLib.heuristic;
       trying to maximize the score, unlike the other who is trying to minimize.
       This will either be an 'x' string or an 'o' string.
 
-      It will return a positive or negative number. 
+      It will return a positive or negative number.
  */
 describe("Testing some basic functionality for the heuristics", function(){
 
@@ -34,12 +34,12 @@ describe("Testing some basic functionality for the heuristics", function(){
 	});
 
 	/* Second, remember that the "maximizing player" and the "minimizing player"
-	   are only maximizing and minimizing players by convention.  They are a 
-	   feature of the map, and not the territory; of how we're looking at 
+	   are only maximizing and minimizing players by convention.  They are a
+	   feature of the map, and not the territory; of how we're looking at
 	   the game, and not the game itself.
 
 	   So if we switch the players, we should get exactly the same value, except
-	   multiplied by -1.*/ 
+	   multiplied by -1.*/
 
 	it("Returns the negative of the value for one player when maximizing player is switched", function(){
 		for(var x = 0; x < 100; x++){
@@ -47,13 +47,13 @@ describe("Testing some basic functionality for the heuristics", function(){
 			for(var z = 0; z < 7; z++){
 				s = s.move( Math.floor(Math.random() * s.width ) )
 			}
-			expect( -heuristic(s, 'x') == heuristic(s, 'o')).to.equal(true);;
+			expect( -heuristic(s, 'x') == heuristic(s, 'o')).to.equal(true);
 		}
 	});
 
-	/* The rest of these all are basically checking to see if 
+	/* The rest of these all are basically checking to see if
 	   the program returns some kind of reasonable values in cases when, although
-	   there are equal numbers of pieces on the board, 'x' has more in a line 
+	   there are equal numbers of pieces on the board, 'x' has more in a line
 	   together than 'o' does. */
 	it("It returns a higher score when 'x' has two in a single line, and 'o' has two disconnected", function(){
 		//Make a new game state
@@ -108,7 +108,7 @@ describe("Testing some basic functionality for the heuristics", function(){
 
 describe('Testing some basic functions in the minimax evaluation function', function(){
 
-	/* The depth which is passed to the minimax function tells it how many 
+	/* The depth which is passed to the minimax function tells it how many
 	   layers down to go.
 
 	   So when it is called with a depth of zero, it should
@@ -136,8 +136,8 @@ describe('Testing some basic functions in the minimax evaluation function', func
 	it('Also returns simply the value of the heuristic function when there are no moves left to make', function(){
 		for(var x = 0; x < 5; x++){
 			//Make a new game state, with a board height of 1 so
-			//that s.nextStates or s.legalMoves returns an array 
-			//of length zero after we've filled the first 
+			//that s.nextStates or s.legalMoves returns an array
+			//of length zero after we've filled the first
 			//and last row entirely.
 			var s = new State({height: 1});
 			s = s.move(0)
@@ -172,10 +172,10 @@ describe('Testing some basic functions in the minimax evaluation function', func
 	});
 
 	/* And sadly, here is where the test specs end.
-	   
+
 	   To see if your algorithm really working any more--that is,
 	   to see if it's making intelligent decisions--you'll need
-	   to play it.  "npm start" and go to localhost:8888 to play 
+	   to play it.  "npm start" and go to localhost:8888 to play
 	   against your algorithm.
      */
 
